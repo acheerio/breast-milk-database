b86f182ec6c28e4922306f386ca29b06d442c9cd
FreedomHat <35280845+FreedomHat@users.noreply.github.com>
2018-05-27 22:13:01 -0500

    Prevent update with blank values

--- public/updatereview.js
+++ public/updatereview.js
@@ -1,10 +1,28 @@
-function updateReview(rid){
-    $.ajax({
-        url: '/review/' + rid,
-        type: 'PUT',
-        data: $('#updatereview').serialize(),
-        success: function(result){
-            window.location.replace("./");
-        }
-    })
+function updateReview(rid) {
+    var reviewTitle = document.getElementById("title").value;
+    var reviewBody = document.getElementById("body").value;
+
+    /*
+    console.log(reviewTitle);
+    console.log(reviewBody);
+    */
+
+    if (reviewTitle.length === 0) {
+        alert("Review Title cannot be empty");
+    }
+    else if (reviewBody.length === 0) {
+        alert("Body cannot be empty");
+    }
+    else {
+        $.ajax({
+            url: '/review/' + rid,
+            type: 'PUT',
+            data: $('#updatereview').serialize(),
+            success: function (result) {
+                window.location.replace("./");
+            }
+        })
+    }
+
+
 };
--- public/updateuser.js
+++ public/updateuser.js
@@ -1,10 +1,56 @@
-function updateUser(uid){
-    $.ajax({
-        url: '/user/' + uid,
-        type: 'PUT',
-        data: $('#updateuser').serialize(),
-        success: function(result){
-            window.location.replace("./");
-        }
-    })
+function updateUser(uid) {
+    var userFname = document.getElementById("fname").value;
+    var userLname = document.getElementById("lname").value;
+    var userEmail = document.getElementById("email").value;
+    var userAddLine1 = document.getElementById("add_line1").value;
+    //var userAddLine2 = document.getElementById("add_line2").value;
+    var userCity = document.getElementById("city").value;
+    var userState = document.getElementById("state").value;
+    var userZip = document.getElementById("zip").value;
+
+
+    /*
+    console.log(userFname);
+    console.log(userLname);
+    console.log(userEmail);
+    console.log(userAddLine1);
+    console.log(userAddLine2);
+    console.log(userCity);
+    console.log(userState);
+    console.log(userZip);
+    */
+
+    if (userFname.length === 0) {
+        alert("First name cannot be empty");
+    }
+    else if (userLname.length === 0) {
+        alert("Last name cannot be empty");
+    }
+    else if (userEmail.length === 0) {
+        alert("Email cannot be empty");
+    }
+    else if (userAddLine1.length === 0) {
+        alert("Address Line 1 cannot be empty");
+    }
+    else if (userCity.length === 0) {
+        alert("City cannot be empty");
+    }
+    else if (userState.length === 0) {
+        alert("State cannot be empty");
+    }
+    else if (userZip.length === 0) {
+        alert("Zip cannot be empty");
+    }
+    else {
+        $.ajax({
+            url: '/user/' + uid,
+            type: 'PUT',
+            data: $('#updateuser').serialize(),
+            success: function (result) {
+                window.location.replace("./");
+
+
+            }
+        })
+    }
 };
