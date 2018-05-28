20000a87b02cd709fb32a7efb41d7d7ad8059919
acheerio <acheerioo@gmail.com>
2018-05-27 23:54:55 -0500

    Update merchant fixes found/made after merge

--- public/updatemerchant.js
+++ public/updatemerchant.js
@@ -19,13 +19,4 @@ function updateMerchant(mid) {
             }
         })
     }
-};function updateMerchant(mid){
-    $.ajax({
-        url: '/merchant/' + mid,
-        type: 'PUT',
-        data: $('#updatemerchant').serialize(),
-        success: function(result){
-            window.location.replace("./");
-        }
-    })
 };
